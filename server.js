280ef6e362f7c73aba89c6ed6f63533e7113f433
shehriar <shehriar1burney@gmail.com>
2024-02-02 02:43:06 +0500

    Adding all existing files

---
+++ server.js
@@ -0,0 +1,51 @@
+// import express from 'express'
+const database = require('./database.js');
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+  
+// handling CORS 
+app.use((req, res, next) => { 
+    res.header("Access-Control-Allow-Origin",  
+               "http://localhost:4200"); 
+    res.header("Access-Control-Allow-Headers",  
+               "Origin, X-Requested-With, Content-Type, Accept"); 
+    next(); 
+}); 
+
+app.get('/api/locations', async (req, res) => { 
+    try {
+        const locations = await database.allLocations();
+        res.json({ locations: locations });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/api/vehicle_models', async (req, res) => {
+    try {
+        const vehicle_models = await database.allCarModels();
+        res.json({vehicle_models: vehicle_models});
+    }
+    catch (error){
+        res.status(500).json({error: error.message});
+    }
+})
+
+app.post('/api/vehicles_from_pickup_location', async (req, res) => {
+    try {
+        const pickupLocation = req.body.pickupLocation;
+        const vehicleDataFromPickupLocation = await database.allVehiclesFromPickupLocation(pickupLocation);
+        res.json({vehicleDataFromPickupLocation: vehicleDataFromPickupLocation});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+  
+app.listen(3000, () => { 
+    console.log('Server listening on port 3000'); 
+});
