c511869137bc550f42b16f2bba79e71a60a48fe6
shehriar <shehriar1burney@gmail.com>
2024-02-07 01:55:02 +0500

    Changed server.js for publishing

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const path = require('path');
 
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, './frontend')));
+app.use(express.static(path.join(__dirname, '/frontend')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/index.html'));
+    res.sendFile(path.join(__dirname, '/frontend/index.html'));
   });
 
 const PORT = process.env.PORT || 3000;
